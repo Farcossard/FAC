954a3a8ad554c35b0d5f89c6a841a6269180aacf
jducos <joel.ducos@etu.u-bordeaux1.fr>
2016-05-24 00:02:50 +0200

    first commit

---
+++ FAC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FAC.Startup))]
+namespace FAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
